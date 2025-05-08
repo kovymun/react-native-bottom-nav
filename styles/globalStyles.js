bf7b8234801a59c8ab097bea04c1f74c8a6580a0
Kovy <codingstudent2021@gmail.com>
2025-05-08 23:05:53 +0200

    Adding Profile Page content - styling to be refined_

---
+++ styles/globalStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 20,
+  },
+  screenTitle: {
+    fontSize: 28,
+    textAlign: "center",
+    marginTop: 24,
+    letterSpacing: 2,
+    fontWeight: 700,
+    color: "#00C569",
+  },
+});
