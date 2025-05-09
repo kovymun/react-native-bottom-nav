7a7bf68dd7e8198af8e63518cb1917fc959cb9f8
Kovy <codingstudent2021@gmail.com>
2025-05-09 14:09:04 +0200

    Applied layout and design to message page

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -27,11 +27,11 @@ const Home = () => {
 
       {/* Card Element */}
       {taskCategories.map(({ id, title, cost, image }) => (
-        <View style={styles.card} key={id}>
+        <View style={globalStyles.card} key={id}>
           <Image style={styles.image} source={image} />
-          <View style={styles.textContainer}>
+          <View style={globalStyles.textContainer}>
             <Text style={styles.title}>{title}</Text>
-            <Text style={styles.cost}>{cost}</Text>
+            <Text style={globalStyles.screenContent}>{cost}</Text>
           </View>
         </View>
       ))}
@@ -47,37 +47,16 @@ const styles = StyleSheet.create({
     paddingHorizontal: 8,
     marginVertical: 24,
   },
-  card: {
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#f6f8f6", // soft light color
-    borderRadius: 16,
-    padding: 8,
-    shadowColor: "#000",
-    shadowOpacity: 0.1,
-    shadowRadius: 8,
-    shadowOffset: { width: 0, height: 2 },
-    elevation: 3,
-    marginBottom: 16,
-  },
+
   image: {
     width: 100,
     height: 100,
     borderRadius: 10,
   },
-  textContainer: {
-    flex: 1,
-    marginLeft: 20,
-    justifyContent: "center",
-  },
+
   title: {
     fontSize: 16,
     fontWeight: "600",
     color: "#002d1d",
-  },
-  cost: {
-    fontSize: 14,
-    color: "#333",
-    marginTop: 4,
   },
 });
--- app/(tabs)/messages.jsx
+++ app/(tabs)/messages.jsx
@@ -1,5 +1,4 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
 import { globalStyles } from "../../styles/globalStyles";
 import { messages } from "../../constants/messageContent";
 
@@ -9,26 +8,18 @@ const Messages = () => {
       <Text style={[globalStyles.screenTitle, { marginBottom: 25 }]}>
         Messages
       </Text>
-      {messages.map(({ id, content }) => (
-        <View
-          key={id}
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            backgroundColor: "#f6f8f6", // soft light color
-            borderRadius: 16,
-            padding: 8,
-            shadowColor: "#000",
-            shadowOpacity: 0.1,
-            shadowRadius: 8,
-            shadowOffset: { width: 0, height: 2 },
-            elevation: 3,
-            marginBottom: 16,
-          }}
-        >
-          <Text style={{ flex: 1, marginLeft: 20, justifyContent: "center" }}>
-            {content}
-          </Text>
+
+      {/* Message History Card */}
+      {messages.map(({ id, handler, content }) => (
+        <View key={id} style={[globalStyles.card, { padding: 15 }]}>
+          {/* Handler Circle */}
+          <View style={styles.handlerCircle}>
+            <Text style={styles.handlerText}>{handler}</Text>
+          </View>
+          {/* Message Content */}
+          <View style={globalStyles.textContainer}>
+            <Text style={globalStyles.screenContent}>{content}</Text>
+          </View>
         </View>
       ))}
     </View>
@@ -37,4 +28,20 @@ const Messages = () => {
 
 export default Messages;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  handlerCircle: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "#b2dfdb",
+    justifyContent: "center",
+    alignItems: "center",
+
+    handlerText: {
+      color: "#004d40",
+      fontWeight: "bold",
+      fontSize: 18,
+      padding: 4,
+    },
+  },
+});
